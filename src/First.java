403e5a91f81384e28d3d9c86175a08155401eeb9
Ramandeep888 <ramandeepchahal37@gmail.com>
2023-07-08 09:03:46 -0400

    java june 2023 basics program

---
+++ src/First.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args){
+        int x;
+        int y;
+        int sum;
+         Scanner sc = new Scanner(System.in);
+        System.out.println("enter first no.");
+        x = sc.nextInt();
+        System.out.println("enter second no. ");
+        y = sc.nextInt();
+        sum = x+ y;
+        System.out.println("the sum of x and y :" + sum );
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/arithmeticoperations.java
@@ -0,0 +1,8 @@
+public class arithmeticoperations {
+    public static void main(String[] args){
+        int x = 6;
+        int y = 7;
+        int result = x+y;
+        System.out.println(result);
+    }
+}
